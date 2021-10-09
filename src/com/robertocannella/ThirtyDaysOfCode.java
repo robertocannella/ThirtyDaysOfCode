47c666997db1a02b09d36c8cefce5ca330d4ac8e
robertocannella <robertocannella@gmail.com>
2021-10-09 15:11:57 -0400

    Day One

---
+++ src/com/robertocannella/ThirtyDaysOfCode.java
@@ -0,0 +1,35 @@
+package com.robertocannella;
+
+import java.util.Scanner;
+
+public class ThirtyDaysOfCode {
+    public static void dayOne(){
+    int i = 4;
+    double d = 4.0;
+    String s = "HackerRank ";
+
+    Scanner scan = new Scanner(System.in);
+
+    /* Declare second integer, double, and String variables. */
+    int a;
+    double b;
+    String c;
+    /* Read and save an integer, double, and String to your variables.*/
+        a = scan.nextInt();
+        b = scan.nextDouble();
+        scan.nextLine();
+        c = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+a);
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d+b);
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+        System.out.println(s+c);
+
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        scan.close();
+}
+}
