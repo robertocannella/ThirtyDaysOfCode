0eb8a4d1470293637dc780d3fc69a97eb8fbc49d
robertocannella <robertocannella@gmail.com>
2021-10-10 10:33:38 -0400

    Added solve() in ThirtyDaysOfCode class.

--- src/com/robertocannella/ThirtyDaysOfCode.java
+++ src/com/robertocannella/ThirtyDaysOfCode.java
@@ -3,10 +3,22 @@ package com.robertocannella;
 import java.util.Scanner;
 
 public class ThirtyDaysOfCode {
-    public static int dayTwo(){
-        return 0;
+    // day two
+    public static void solve(double mealCost, int tipPercent, int taxPercent) {
+        // Write your code here
+        double tipAmount = ((double) tipPercent/100) * mealCost;
+        double taxAmount = ((double)taxPercent/100) * mealCost;
+        tipAmount = Math.round(tipAmount*100)/100d;
+        taxAmount = Math.round(taxAmount*100)/100d;
+        double total = (mealCost + tipAmount + taxAmount);
+        total = Math.round(total);
+        //System.out.println((double) 20/100);
+        System.out.println(tipAmount);
+        System.out.println(taxAmount);
+        System.out.println(total);
+        System.out.println(taxAmount+tipAmount+mealCost);
     }
-
+    // day one
     public static void dayOne(){
     int i = 4;
     double d = 4.0;
