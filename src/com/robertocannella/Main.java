1d0e5ee7c2cb499f981ca8d511d7710c99b7b0af
robertocannella <robertocannella@gmail.com>
2021-10-21 19:39:38 -0400

    Added abstract Book class, MyBook class to ThirtyDaysOfCode class.

--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -6,11 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] scores = {41, 42, 43, 44, 45, 46, 48};
-        ThirtyDaysOfCode.Student s = new ThirtyDaysOfCode.Student("Roberto","cannella",234,scores);
-
-        //s = new ThirtyDaysOfCode.Student("Roberto", "Cannella", 89234, scores);
-        System.out.println(s.calculate());
     }
 
 }
--- src/com/robertocannella/ThirtyDaysOfCode.java
+++ src/com/robertocannella/ThirtyDaysOfCode.java
@@ -9,6 +9,31 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 public class ThirtyDaysOfCode {
+    // day thirteen
+    abstract class Book {
+        String title;
+        String author;
+
+        Book(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
+
+        abstract void display();
+    }
+    class MyBook extends Book {
+        int price;
+        MyBook(String title, String author, int price) {
+            super(title, author);
+            this.price = price;
+        }
+
+        public void display(){
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("Price: " + price);
+        }
+    }
     // day twelve
     public static class Student extends Person{
         private int[] testScores;
