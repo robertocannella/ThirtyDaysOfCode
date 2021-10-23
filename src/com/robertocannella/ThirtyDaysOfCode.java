64bd2cece90ed1cc4b3c3dac9d145b82d4444a10
robertocannella <robertocannella@gmail.com>
2021-10-23 14:41:56 -0400

    Added insert() to ThirtyDaysOfCode class.

--- src/com/robertocannella/ThirtyDaysOfCode.java
+++ src/com/robertocannella/ThirtyDaysOfCode.java
@@ -9,6 +9,31 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 public class ThirtyDaysOfCode {
+    // day fifteen
+    public static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+    public static  Node insert(Node head,int data) {
+        //Complete this method
+        if (head == null){
+            return new Node(data);
+        }
+
+        Node current = head;
+        Node temp = new Node(data);
+        while(current.next != null){
+            current = current.next;
+
+        }
+        current.next = temp;
+
+        return head;
+    }
     // day fourteen
     class Difference {
         private int[] elements;
