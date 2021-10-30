8c3e32319f9bec3841f15412356d3cfa359bfe85
robertocannella <robertocannella@gmail.com>
2021-10-30 13:51:06 -0400

    Added generic Tree Class with accompanying Methods to ThirtyDaysOfCode class.

--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -12,7 +12,7 @@ public class Main {
         ThirtyDaysOfCode.EmptyBST<Integer> tree = new ThirtyDaysOfCode.EmptyBST<>();
         var tree2 = tree.add(23);
         System.out.println("done");
-        var height = tree2.getHeight(tree2);
+
 
     }
 }
